7a22e15981c4b49de5394e2ce91d183941f2f203
wjhames <williamjasonhames@gmail.com>
2022-08-21 15:48:05 -0500

    Updated repository to newest version of Crystal

--- provisions/bootstrap.sh
+++ provisions/bootstrap.sh
@@ -9,17 +9,18 @@ echo -e "\e[92mInstalling Curl\e[0m"
 apt -y install curl
 
 echo -e "\e[92mAdding Repository\e[0m"
-curl https://dist.crystal-lang.org/apt/setup.sh | sudo bash
+echo 'deb http://download.opensuse.org/repositories/devel:/languages:/crystal/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/devel:languages:crystal.list
+curl -fsSL https://download.opensuse.org/repositories/devel:languages:crystal/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_languages_crystal.gpg > /dev/null
+apt update
 
 echo -e "\e[92mInstalling Crystal Language\e[0m"
-sudo apt -y install build-essential
-sudo apt -y install crystal
+apt -y install crystal
 
 echo -e "\e[92mInstalling Recommended Packages for Crystal\e[0m"
-sudo apt install libssl-dev
-sudo apt install libxml2-dev
-sudo apt install libyaml-dev
-sudo apt install libgmp-dev
-sudo apt install libz-dev
+apt install libssl-dev
+apt install libxml2-dev
+apt install libyaml-dev
+apt install libgmp-dev
+apt install libz-dev
 
 echo -e "\e[92mSetup complete\e[0m"
